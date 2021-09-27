9b44cdfa76e10065a46635387c7f946afd3387be
Meet Patel <bobmeet1308@gmail.com>
2021-09-27 02:11:42 -0500

    Tweets from the clone app shows on my Twitter account.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,10 +3,18 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
 
@@ -16,11 +24,15 @@ public class ComposeActivity extends AppCompatActivity {
 
     EditText etCompose;
     Button btnTweet;
+    public static final String TAG = "ComposeActiviry";
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getRestClient(this);
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
@@ -35,11 +47,28 @@ public class ComposeActivity extends AppCompatActivity {
                     Toast.makeText(ComposeActivity.this, "Your tweet content cannot be empty", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if(tweetContent.length() > MAX_TWEET_LENGTH){
+                if(tweetContent.length() > MAX_TWEET_LENGTH ){
                     Toast.makeText(ComposeActivity.this, "Your tweet content is too long", Toast.LENGTH_LONG).show();
                     return;
                 }
                 Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.d(TAG, "OnSuccess publishing tweet");
+                        try {
+                            Tweet.fromJson(json.jsonObject);
+                            Log.d(TAG, "Published tweet says: " + tweetContent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.d(TAG, "OnFailure publishing tweet", throwable);
+                    }
+                });
             }
         });
         //make an API call to Twitter to publish the tweet
